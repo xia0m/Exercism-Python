f24a61e1adef25704d55554f0153de95532fe85a
xia0m <alexma325@gmail.com>
2019-04-22 22:15:41 -0700

    imporved pangram exercise

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,8 +1,12 @@
-alphabetRef = "abcdefghijklmnopqrstuvwxyz"
+# alphabetRef = "abcdefghijklmnopqrstuvwxyz"
 
+# def is_pangram(sentence):
+# 	sentenceSet = set()
+# 	for x in sentence.lower():
+# 		if x in alphabetRef:
+# 			sentenceSet.add(x)
+# 	return len(sentenceSet) == len(alphabetRef)
+
+#### improved version ####
 def is_pangram(sentence):
-	sentenceSet = set()
-	for x in sentence.lower():
-		if x in alphabetRef:
-			sentenceSet.add(x)
-	return len(sentenceSet) == len(alphabetRef)
+	return set(sentence.lower())>=set('abcdefghijklmnopqrstuvwxyz')
